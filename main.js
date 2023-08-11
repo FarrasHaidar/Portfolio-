9bc478350c8b221e95f809adc71beb29ef40b088
FarrasHaidar <farrashaidar27@gmail.com>
2023-08-11 20:50:32 +0700

    add about

---
+++ main.js
@@ -0,0 +1,14 @@
+function toggleDetail(ev) {
+    const target = $(ev.target)
+
+    if($(target).hasClass("active")){
+        $(target).html("More Info").removeClass("active")
+    }else {
+        $(target).html("Less Info").addClass("active")
+    }
+
+    const item = ($(target).parents(".about-exp-item"))
+    const detail = $(item).children(".about-exp-item-detail")
+
+    $(detail).slideToggle()
+}
